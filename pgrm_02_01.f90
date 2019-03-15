c01eaa5847d3a2ca0c2666a01c4a21692f438c67
Justin <jdebow@ucmerced.edu>
2019-03-15 02:47:46 -0700

    Program 1 finished

--- pgrm_02_01.f90
+++ pgrm_02_01.f90
@@ -28,10 +28,12 @@
       Read(IIn,*) NDim
       Allocate(Array_Input(NDim*NDim),Matrix(NDim,NDim))
 !
-! *************************************************************************
-! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
-! *************************************************************************
+!     This will read the array elements from the input file based off of
+!     the labels above. 
 !
+      do i=1,NDim*NDim
+      read (IIn,*)Array_Input(i)
+      enddo
 !
 !     Convert Array_Input to Matrix and print the matrix.
 !
@@ -64,11 +66,13 @@
 !     Loop through the elements of AMatOut and fill them appropriately from
 !     Array_Input.
 !
-!
-! *************************************************************************
-! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
-! *************************************************************************
-!
+      k=1
+      do i=1,N
+      do j=1,M
+      AMatOut(j,i)=Arrayin(k)
+      k=k+1
+      enddo
+      enddo
 !
       Return
       End Subroutine Packed2Matrix_ColumnWise
@@ -91,11 +95,51 @@
 !     Loop through the elements of AMatOut and fill them appropriately from
 !     Array_Input.
 !
-!
-! *************************************************************************
-! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
-! *************************************************************************
-!
+      k=1
+      do i=1,N
+      do j=1,M
+      AMatOut(j,i)=Arrayin(k)
+      k=k+1
+      enddo
+      enddo
 !
       Return
       End Subroutine Packed2Matrix_RowWise
+!
+      Subroutine Print_Matrix_Full_Real(AMat,M,N)
+!
+!     This subroutine prints a real matrix that is fully dimension -
+!     i.e.,
+!     not stored in packed form. AMat is the matrix, which is
+!     dimensioned
+!     (M,N).
+!
+!     The output of this routine is sent to unit number 6 (set by the
+!     local
+!     parameter integer IOut).
+!
+!
+!     Variable Declarations
+!
+      implicit none
+      integer,intent(in)::M,N
+      real,dimension(M,N),intent(in)::AMat
+!
+!     Local variables
+      integer,parameter::IOut=6,NColumns=5
+      integer::i,j,IFirst,ILast
+!
+ 1000 Format(1x,A)
+ 2000 Format(5x,5(7x,I7))
+ 2010 Format(1x,I7,5F14.6)
+!
+      Do IFirst = 1,N,NColumns
+        ILast = Min(IFirst+NColumns-1,N)
+        write(IOut,2000) (i,i=IFirst,ILast)
+        Do i = 1,M
+          write(IOut,2010) i,(AMat(i,j),j=IFirst,ILast)
+        endDo
+      endDo
+!
+      Return
+      End Subroutine Print_Matrix_Full_Real
