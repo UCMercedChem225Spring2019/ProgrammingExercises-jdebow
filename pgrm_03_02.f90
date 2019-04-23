4af513f0bdd651a19f0779c9a9be2ca56e0af656
Justin <jdebow@ucmerced.edu>
2019-04-22 20:48:23 -0700

    All programs finished

--- pgrm_03_02.f90
+++ pgrm_03_02.f90
@@ -50,3 +50,57 @@
       endDo
 !
       end program pgrm_03_02
+!
+       Subroutine Print_Matrix_Full_Real(AMat,M,N)
+!
+!     This subroutine prints a real matrix that is fully dimension -
+!     i.e.,
+!     not stored in packed form. AMat is the matrix, which is
+!     dimensioned
+!     (M,N).
+!
+!     The output of this routine is sent to unit number 6 (set by the
+!     local
+!     parameter integer IOut).
+!
+!
+!     Variable Declarations
+!
+      implicit none
+      integer,intent(in)::M,N
+      real,dimension(M,N),intent(in)::AMat
+!
+!     Local variables
+      integer,parameter::IOut=6,NColumns=5
+      integer::i,j,IFirst,ILast
+!
+ 1000 Format(1x,A)
+ 2000 Format(5x,5(7x,I7))
+ 2010 Format(1x,I7,5F14.6)
+!
+      Do IFirst = 1,N,NColumns
+        ILast = Min(IFirst+NColumns-1,N)
+        write(IOut,2000) (i,i=IFirst,ILast)
+        Do i = 1,M
+          write(IOut,2010) i,(AMat(i,j),j=IFirst,ILast)
+        endDo
+      endDo
+!
+      Return
+      End Subroutine Print_Matrix_Full_Real
+!
+      subroutine Sq2SymMatrix(nDim,SqMatrix,SymMatrix)
+!
+      integer:: i,j,k,NDim
+      real,dimension(nDim):: SymMatrix
+      real,dimension(nDim, nDim):: SqMatrix
+!
+      k = 0
+      do j=1,nDim
+      do i=1,j
+      k = k + 1
+      SymMatrix(k) = SqMatrix(i,j)
+      enddo
+      enddo
+!
+      end subroutine Sq2SymMatrix
